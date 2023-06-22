2ddf879df2f6e8dae88119de15481ea42ba5ca31
minechoo <choominhae@gmail.com>
2023-06-22 12:31:28 +0900

    noArg

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@
 import './scss/style.scss';
 
 function App() {
+	const noArg = (e) => {
+		e.target.innerText = 'noname';
+	};
 	const handleClick = (e, txt) => {
 		e.target.innerText = txt;
 	};
@@ -21,8 +24,11 @@ function App() {
 			>
 				button1
 			</button>
+
 			{/* 인수를 전달해야되는 함수를 이벤트 연결할때는 wrapping함수로 아래처럼 호출 */}
 			<button onClick={(e) => handleClick(e, '안녕')}>button2</button>
+
+			<button onClick={noArg}>button3</button>
 		</div>
 	);
 }
