edee9c4eff855b57d109adf3202e594f56c124e1
minechoo <choominhae@gmail.com>
2023-06-22 12:57:15 +0900

    배열을 통한 JSX 동적요소

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import './scss/style.scss';
 
 function App() {
-	const boxStyle = {
-		width: 300,
-		height: 300,
-		backgroundColor: 'grey',
-	};
-
-	const colorChange = (e, color) => {
-		e.target.style.backgroundColor = color;
-	};
+	const colors = ['aqua', 'lightgreen', 'hotpink', 'orange', 'skyblue'];
 
 	return (
 		<div className='wrap'>
-			<div className='box' style={boxStyle} onClick={(e) => colorChange(e, 'blue')}></div>
+			<ul>
+				{colors.map((color, idx) => {
+					return <li key={idx}>{color}</li>;
+				})}
+			</ul>
 		</div>
 	);
 }
 
 export default App;
+
+/**
+ * 불변성(immutable) : 원형을 변형시키지 않으면서 원본을 유지한채로 복사본을 변경루 비교하는 형태
+ * -리액트에서는 원본대비 변경된 부분을 비교해서 화면은 랜더링하기때문데 불변성유지가 필수
+ * - 리액트에서는 원본과 복사본이 있어야함
+ * - 참조형 자료를 변경할때에는 무조건 전개연산자로 deep copy
+ * - 특정요소를 반복처리하기 위해서는 무조건 map처리
+ * - JSX문법 안쪽에서는 map외의 반복문 사용불가
+ * - JSX로 반복되는 요소를 동적으로 만들때에는 무조건 key값으로 고유값을 등록(리액트로 하여금 반복도는 요소를 개별적으로 인지시키기 위한)
+ */
