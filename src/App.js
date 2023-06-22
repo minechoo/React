9f7e2c2683c57ee1997678abc2464fb0e45f5a63
minechoo <choominhae@gmail.com>
2023-06-22 12:42:54 +0900

    과제

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
-/* import Header from './Header';
-   import Footer from './Footer';
-	 import Section from './Section';*/
-//import { Header, Footer, Section } from './Common';
-// const name = '김영희';
-// const class1 = 'box';
-
 import './scss/style.scss';
 
 function App() {
-	const noArg = (e) => {
-		e.target.innerText = 'noname';
+	const boxStyle = {
+		width: 300,
+		height: 300,
+		backgroundColor: 'grey',
 	};
-	const handleClick = (e, txt) => {
-		e.target.innerText = txt;
+
+	const colorChange = (e, color) => {
+		e.target.style.backgroundColor = color;
 	};
+
 	return (
 		<div className='wrap'>
-			<button
-				onClick={(e) => {
-					console.log('clicked');
-					e.target.innerText = '버튼';
-				}}
-			>
-				button1
-			</button>
-
-			{/* 인수를 전달해야되는 함수를 이벤트 연결할때는 wrapping함수로 아래처럼 호출 */}
-			<button onClick={(e) => handleClick(e, '안녕')}>button2</button>
-
-			<button onClick={noArg}>button3</button>
+			<div className='box' style={boxStyle} onClick={(e) => colorChange(e, 'blue')}></div>
 		</div>
 	);
 }
